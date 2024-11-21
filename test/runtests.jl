34c1ca8e5fdd72c1e1d7b3e89bf9304d485aa70e
TommasoStentella <tommaso.stentella@gmail.com>
2024-11-21 16:30:05 +0100

    update DemoInfer dependency

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using DemoInfer, PyPlot, HistogramBinnings, StatsBase
 @testset "DemoPlots.jl" begin
     h = Histogram(LogEdgeVector(lo = 30, hi = 1_000_000, nbins = 200));
     get_sim!([1_000_000_000, 10_000, 2_000, 2_000, 5_000, 10_000], h, 2.36e-8, 1e-8)
-    res = sequential_fit(h, 2.36e-8, 4)
+    res = sequential_fit(h, 2.36e-8, 4, 1_000_000_000)
 
     @test isa(plot_hist(h, s=4, c="blue"), PyPlot.PyObject)
 
@@ -14,5 +14,5 @@ using DemoInfer, PyPlot, HistogramBinnings, StatsBase
     @test isa(plot_naive_residuals(h, res[end], 2.36e-8), PyPlot.PyObject)
 
     _, ax = subplots(figsize=(7, 5))
-    @test isnothing(plot_demography(res[end], ax))
+    @test isa(plot_demography(res[end], ax), PyPlot.PyObject)
 end
